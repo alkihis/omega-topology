8d5487cfc646cc812e09aae187968769851d53e4
lberanger <louis.beranger@etu.univ-lyon1.fr>
2019-07-18 10:31:55 +0200

    Graph data dl

--- src/components/omega-download/omega-download.tsx
+++ src/components/omega-download/omega-download.tsx
@@ -1,4 +1,5 @@
-import { Component, h, Element, Event, EventEmitter } from '@stencil/core';
+import { Component, h, Element, Event, EventEmitter, State } from '@stencil/core';
+import FrontTopology from '../../utils/FrontTopology';
 
 @Component({
   tag: "omega-download",
@@ -9,6 +10,8 @@ export class OmegaTrim {
   @Element() el: HTMLElement;
 
   public static readonly tag = "omega-download";
+
+  @State() render_trigger: number;
 
   @Event({
     eventName: "omega-download.download"
@@ -33,13 +36,74 @@ export class OmegaTrim {
       this.fileJSON.emit(e.value);
   }
 
+  generatePruneParameters() {
+    const trim_p = FrontTopology.current_trim_parameters;
+    const prune_p = FrontTopology.current_prune_parameters;
+    let data: any;
+
+    if (prune_p) {
+      data = prune_p;
+    }
+    else if ('custom_prune' in trim_p && trim_p.custom_prune) {
+      data = trim_p.custom_prune;
+    }
+    else {
+      return <li>You don't have any current prune parameters</li>;
+    }
+
+    return [
+      <li>Maximum distance: {data[1]}</li>, 
+      <li>Seed(s): {data[0].length} selected</li>
+    ];
+  }
+
+  generateTrimParameters() {
+    const trim_p = FrontTopology.current_trim_parameters;
+
+    const binding = {
+      identity: "Identity",
+      similarity: "Similarity",
+      coverage: "Coverage",
+      experimental_detection_method: "Experimental detection methods",
+      taxons: "Taxons",
+      e_value: "E-value"
+    };
+
+    const items = [];
+
+    if (Object.keys(trim_p).length === 0) {
+      return <li>You don't have any current trimming parameters</li>;
+    }
+    
+    for (const t in trim_p) {
+      const data = trim_p[t];
+      const title = binding[t];
+
+      if (t === 'custom_prune') {
+        continue;
+      }
+      else if (!data || (Array.isArray(data) && data.length === 0)) {
+        continue;
+      }
+      else {
+        items.push(
+          <li>
+            {title}: {Array.isArray(data) ? String(data.length) + " selected" : data.toString()}
+          </li>
+        );
+      }
+    }
+
+    return items;
+  }
+
   /**
    * Rendu graphique.
    */
   render() {
     return (
       <div>     
-        <div class="download-container" title="Download graph" data-toggle="modal" data-target="#modalDownload">
+        <div class="download-container" title="Download graph" data-toggle="modal" data-target="#modalDownload" onClick={() => this.render_trigger = Math.random()}>
           <i class="material-icons">get_app</i>
         </div>
 
@@ -55,6 +119,14 @@ export class OmegaTrim {
               </div>
 
               <div class="modal-body">
+                <p>
+                  Current graph have {FrontTopology.topo ? FrontTopology.topo.nodeNumber : 0} nodes and {FrontTopology.topo ? FrontTopology.topo.edgeNumber : 0} links.
+                  <br/>
+                  Defined trimming parameters are:
+                  <ul>{this.generateTrimParameters()}</ul>
+                  Defined prune parameters:
+                  <ul>{this.generatePruneParameters()}</ul>
+                </p>
                 <p>
                   File will be downloaded with the name given in this input.
                   <br/>
